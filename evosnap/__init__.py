38250702e0cc4f8beff5570bf1b18d5d707bb77f
jros <jros@zertifica.com>
2017-02-20 12:39:34 +0100

    fixed bug

--- evosnap/__init__.py
+++ evosnap/__init__.py
@@ -4,3 +4,5 @@ from evosnap.response import Response
 from evosnap.transactions import Transaction, TransactionTenderData, TransactionData, CardData
 from evosnap.transactions import Capture, CardSecurityData, EcommerceSecurityData, InternetTransactionData
 from evosnap.transactions import InternationalAVSData, ReturnById, Adjust, Addendum, Unmanaged, Undo
+from evosnap.transactions import RequestType, TransactionType, TransactionDataType, CardType, GoodsType, CustomerPresent, \
+    IndustryType, EntryMode, AccountType, CVDataProvided, TokenIndicator, ChargeType, PINDebitUndoReason, UndoReason
